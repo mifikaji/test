76f537ebf4cf43fcde4cd76bd68cd14eaa601f57
mifikaji <82213199+mifikaji@users.noreply.github.com>
2021-05-06 15:14:31 +0300

    Update test2.js

--- test2.js
+++ test2.js
@@ -5,66 +5,69 @@
 // @description  try to take over the world!
 // @author       You
 // @match        https://yandex.ru/*
-// @match        https://xn----7sbab5aqcbiddtdj1e1g.xn--p1ai/*
-// @icon         
+// @match		 https://napli.ru/*
+// @icon
 // @grant        none
 // ==/UserScript==
-let keywords=["гобой","как звучит саксофон","как звучит флейта"];
 
-let button_search=document.getElementsByClassName("button_theme_search")[0];
-let links=document.links;
-let keyword=keywords[getRandom(0,keywords.length)];
-let yandexInput=document.getElementsByName('text')[0];
-let i=0;
+let keywords = ["вывод произвольных полей wordpress", "pods wordpress", "napli.ru", "10 самых популярных шрифтов от Yandex", "Отключение редакций и ревизий в WordPress"];
 
-if(button_search!==undefined){
-    let timerId=setInterval(function(){
-        yandexInput.value+=keyword[i];
-        i++;
-        if(i==keyword.length){
-            clearInterval(timerId);
-            button_search.click();
-        }
-    },1000);
-}else if(location.hostname=="xn----7sbab5aqcbiddtdj1e1g.xn--p1ai"){
-    console.log("Мы на музыкалке");
-    setTimeout(()=>{
-        let index=getRandom(0,links.length);
-        if(getRandom(0,101)>=60){
-            location.href="https://yandex.ru/";
-        }
-        if(links[index].href.indexOf("xn----7sbab5aqcbiddtdj1e1g.xn--p1ai")!=-1){
-            links[index].click()
-        }
-    },getRandom(3500,7000));
+let btn = document.getElementsByClassName('button mini-suggest__button')[0];
+let links = document.links;
+let keyword = keywords[getRandom(0,keywords.length)];
+let YandexInput = document.getElementsByName('text')[0];
+let i = 0;
+
+
+if(btn !== undefined){
+	let timerId = setInterval(()=> {
+		YandexInput.value += keyword[i];
+		i++;
+		if(i == keyword.length) {
+			clearInterval(timerId);
+			btn.click();
+		}
+	}, 1000);
+
+
+
+}else if(location.hostname == "napli.ru" ) {
+	console.log("Мы на napli.ru");
+	setTimeout(()=>{
+		let index = getRandom(0,links.length);
+
+		if(getRandom(0,101)>=70) {
+			location.href = "https://yandex.ru/";
+		}
+		if(links[index].href.indexOf('napli.ru')!=-1)
+			links[index].click();
+	},getRandom(2000,3500));
 }
 else{
-    let nextYandexPage=true;
-    for(let i=0; i<links.length;i++){
-        if(links[i].href.indexOf(`xn----7sbab5aqcbiddtdj1e1g.xn--p1ai`)!==-1){
-            let link=links[i];
-            nextYandexPage=false;
-            link.removeAttribute("target");
-            console.log("Нашёл ссылку "+link);
-            setTimeout(()=>{
-                link.click();
-            },getRandom(1000,5000));
-            break;
-        }
-    }
-    if(document.querySelector("").innerText=="5"){
-        nextYandexPage=false;
-        location.href="https://yandex.ru/";
-    }
+	let nextYandexPage = true;
+	for(let i=0; i<links.length; i++) {
+		if(links[i].href.indexOf('napli.ru')!=-1) {
+			let link = links[i];
+			nextYandexPage = false;
+			console.log("Нашел фразу" + link);
+			setTimeout(()=>{
+				link.click();
+            },getRandom(1000,4500));
+			break;
+		}
+	}
+	if(document.querySelector('.pager__item_current_yes').textContent == "5") {
+		nextYandexPage = false;
+		location.href = "https://yandex.ru/";
+	}
 
-    if(document.querySelector(".pager__item_current_yes").innerText!=="5"){
-        setTimeout(()=>{
-            let next=document.getElementByClassName("pager__item_kind_next");
-            next.click();
-        },getRandom(3000,5500));
-    }
+	if(document.querySelector('.pager__item_current_yes').textContent !== "5") {
+		setTimeout(()=>{
+			document.querySelector('.pager__item_kind_next').click();}
+				   ,getRandom(3000,5000));
+	}
 }
 
-function getRandom(min,max){
-    return Math.floor(Math.random()*(max-min)+min);
+function getRandom(min,max) {
+	return Math.floor(Math.random()*(max-min)+min);
 }
